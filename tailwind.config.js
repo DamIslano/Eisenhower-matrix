503434d6280946240aeccee1d4d52b0ba2815bcd
iladyka <dev.ivan.ladyka@gmail.com>
2024-05-27 22:19:15 +0300

    feat: custom colors | box shadow | text shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import textShadow from 'tailwindcss-textshadow';
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -9,8 +10,25 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          DEFAULT: '#4969DB',
+          light: '#6481D9',
+          dark: '#7064D9'
+        }
+      },
+      boxShadow: {
+        'box-shadow-inset': 'inset 20px 20px 20px #6481D9',
+        'box-shadow-inset-reverse': 'inset -20px 20px 20px #6481D9',
+        'box-shadow-inset-negative': 'inset 20px -20px 20px #6481D9',
+        'box-shadow-inset-negative-reverse': 'inset -20px -20px 20px #6481D9',
+      },
+      textShadow: {
+        purple: '1px 1px #4969DB'
+      }
+    },
   },
-  plugins: [],
+  plugins: [textShadow],
 }
 
