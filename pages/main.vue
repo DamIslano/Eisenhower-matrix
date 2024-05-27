d22f7f5df38126ea00e19d584497ee23cf7fbf82
iladyka <dev.ivan.ladyka@gmail.com>
2024-05-27 12:42:35 +0300

    feat: graph add

--- pages/main.vue
+++ pages/main.vue
@@ -3,10 +3,13 @@
 </script> -->
 
 <template>
-  <div class="py-24 sm:py-32">
+  <div class="pt-24 sm:pt-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl sm:text-center">
+      <div class="mx-auto max-w-2xl text-center">
         <h2 class="text-3xl font-bold tracking-tight text-yellow-300 sm:text-4xl">Eisenhower Matrix</h2>
+      </div>
+      <div class="flex items-center justify-center">
+        <img src="../assets/images/graph.png" alt="" class="w-4/5 object-cover">
       </div>
     </div>
   </div>
